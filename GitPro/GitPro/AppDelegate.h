234a7084bc4d44cfe541d2642d0d332ed2ec20ae
asheng <1720470516@qq.com>
2016-04-28 11:15:00 +0800

    创建了新项目

---
+++ GitPro/GitPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitPro
+//
+//  Created by apple on 16/4/28.
+//  Copyright © 2016年 LZS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
